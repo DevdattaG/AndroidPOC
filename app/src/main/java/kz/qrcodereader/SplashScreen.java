7d2d78ffdcf0998ed2c12fe08f0395c43ec0fdc7
Devdatta <devdatta.godbole777@gmail.com>
2016-07-19 18:01:21 +0530

    raw splash screen addition

---
+++ app/src/main/java/kz/qrcodereader/SplashScreen.java
@@ -0,0 +1,37 @@
+package kz.qrcodereader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by KZ-Tech on 7/19/2016.
+ */
+public class SplashScreen extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(10000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
